494c99d6513610a2a77cb6887e37d567090e66d4
EmilTol <erazertt@gmail.com>
2025-02-25 14:52:26 +0100

    DatabaseConnection.java

---
+++ src/main/java/com/example/salonprojekt/DatabaseConnection.java
@@ -0,0 +1,22 @@
+package com.example.salonprojekt;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+class DatabaseConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/salon";
+
+    private static final String USER = "root";
+
+    private static final String PASSWORD = "root";
+
+    public static Connection getconnection(){
+        try {
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.salonprojekt {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
 
     opens com.example.salonprojekt to javafx.fxml;
