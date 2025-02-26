78730a15c1a5d1df56a92ae5f90df5b3a43e16dd
Annika <annikarasmussen93@gmail.com>
2025-02-26 16:57:32 +0100

    CheckBox kommentar

--- src/main/java/com/example/salonprojekt/TableController.java
+++ src/main/java/com/example/salonprojekt/TableController.java
@@ -49,6 +49,7 @@ public class TableController extends BaseController implements Initializable {
     private TableColumn<Table, String> status;
 
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        //en listerner holder vist øje med når der sker ændringer. holder TableView opdateret
         myCheckBox.selectedProperty().addListener((observable, oldValue, newValue) -> {
             updateTableData(newValue);
         });
@@ -59,6 +60,7 @@ public class TableController extends BaseController implements Initializable {
 
     private void updateTableData(boolean isChecked) {
         TableDatabaseHandler tableDatabaseHandler = new TableDatabaseHandler();
+        //den her linje ligger gettingOtherTable og gettingTable hvilken en der køre er styret af CheckBox
         ObservableList<Table> tableData = isChecked ? tableDatabaseHandler.gettingOtherTable() : tableDatabaseHandler.gettingTable();
 
         System.out.println("Antal rækker hentet: " + tableData.size());
