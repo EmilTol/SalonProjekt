694ec4ea4f2f8e23c0da138eaf3d68e30f420c6f
EmilTol <erazertt@gmail.com>
2025-03-02 11:46:58 +0100

    Ændret navn på main slettet ligegyldige ting

--- src/main/java/com/example/salonprojekt/Main.java
+++ src/main/java/com/example/salonprojekt/Main.java
@@ -1,13 +1,11 @@
 package com.example.salonprojekt;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         SceneManager sceneManager = new SceneManager(stage);
